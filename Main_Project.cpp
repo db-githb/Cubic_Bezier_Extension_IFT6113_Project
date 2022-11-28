52f87c4f4f1152741859d464cfe0d3028ca0ab88
D.B <snotboogieisdead@gmail.com>
2022-11-27 23:39:39 -0500

    bezier interpolation working, no blend

--- Main_Project.cpp
+++ Main_Project.cpp
@@ -11,35 +11,45 @@ double  getPt(double n1, double n2, double perc)
     return n1 + (diff * perc);
 }
 
-// from https://stackoverflow.com/questions/785097/how-do-i-implement-a-b%C3%A9zier-curve-in-c
-void qBezierInterpolation(int** image,  std::vector<double> p0, std::vector<double> p1, std::vector<double> p2) {
-    for (double i = 0; i < 1; i += 0.001)
-    {
-        double x1 = p0[0];
-        double y1 = p0[1];
-        
-        double x3 = p2[0];
-        double y3 = p2[1];
+std::vector<std::vector<double>> qBezierParams(std::vector<double> p0, std::vector<double> p1, std::vector<double> p2) {
+    std::vector<std::vector<double>> bVec(3);
 
-        // calculating b
-        double t = .5;
-        double x2 = (p1[0] - (1 - t) * (1 - t) * x1 - t * t * x3)/(2*(1-t)*t);
-        double y2 = (p1[1] - (1 - t) * (1 - t) * y1 - t * t * y3) / (2 * (1 - t) * t);
+    bVec[0] = p0;
+    bVec[2] = p2;
 
-        // The Green Line
-       double xa = getPt(x1, x2, i);
-       double ya = getPt(y1, y2, i);
-       double xb = getPt(x2, x3, i);
-       double yb = getPt(y2, y3, i);
+    // calculating b
+    double t = .5;
+    double x2 = (p1[0] - (1 - t) * (1 - t) * p0[0] - t * t * p2[0]) / (2 * (1 - t) * t);
+    double y2 = (p1[1] - (1 - t) * (1 - t) * p0[1] - t * t * p2[1]) / (2 * (1 - t) * t);
+    bVec[1] = { x2, y2 };
 
-        // The Black Dot
-       int x = floor(getPt(xa, xb, i));
-       int y = floor(getPt(ya, yb, i));
-
-       image[x][y] = 1;
-    }
+    return bVec;
 }
 
+// from https://stackoverflow.com/questions/785097/how-do-i-implement-a-b%C3%A9zier-curve-in-c
+std::vector<double> qBezierInterpolation(std::vector<std::vector<double>> bVec, double t) {
+        // The Green Line
+       double xa = getPt(bVec[0][0], bVec[1][0], t);
+       double ya = getPt(bVec[0][1], bVec[1][1], t);
+       double xb = getPt(bVec[1][0], bVec[2][0], t);
+       double yb = getPt(bVec[1][1], bVec[2][1], t);
+
+        // The Black Dot
+       double x = getPt(xa, xb, t);
+       double y = getPt(ya, yb, t);
+
+       return { x,y };
+}
+
+std::vector<int> blend(std::vector<std::vector<double>> bVec1, std::vector<std::vector<double>> bVec2, double t) {
+    std::vector<double> p1 = qBezierInterpolation(bVec1, t);
+    std::vector<double> p2 = qBezierInterpolation(bVec2, t);
+
+    double x = cos(M_PI_2 * t) * cos(M_PI_2 * t) * p1[0] + sin(M_PI_2 * t) * sin(M_PI_2 * t) * p2[0];
+    double y = cos(M_PI_2 * t) * cos(M_PI_2 * t) * p1[1] + sin(M_PI_2 * t) * sin(M_PI_2 * t) * p2[1];
+
+    return { (int) floor(x), (int) floor(y) };
+}
 
 int main() {
     std::vector<std::vector<double>> cp = { {42, 42}, { 84,84 }, { 126, 42 }, { 168, 126 }};
@@ -51,10 +61,18 @@ int main() {
 			image[i][j] = 2;
 		}
 	}
-    
+    std::vector<std::vector<double>> bVec1 = qBezierParams(cp[0], cp[1], cp[2]);
+    std::vector<std::vector<double>> bVec2 = qBezierParams(cp[1], cp[2], cp[3]);
 
+    for (double t = 0; t <= 1; t += .001) {
+        //std::vector<int> point = blend(bVec1, bVec2, t);
+        std::vector<double> p1 = qBezierInterpolation(bVec1, t);
+        std::vector<double> p2 = qBezierInterpolation(bVec2, t);
 
-    qBezierInterpolation(image, cp[0], cp[1], cp[2]);
+        image[(int) p1[0]][(int) p1[1]] = 1;
+        image[(int) p2[0]][(int) p2[1]] = 1;
+
+    }
 
     // color control points
     for (int i = 0; i < cp.size(); i++) {
